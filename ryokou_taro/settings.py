f05a678e4e09587fff4187c31315d35bef36f540
khuctienminh <ykh2035145@stu.o-hara.ac.jp>
2021-12-01 10:16:03 +0900

    sua tz

--- ryokou_taro/settings.py
+++ ryokou_taro/settings.py
@@ -111,13 +111,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
